2146dadb132b822eb5193b2a1eaa0694f2181b61
SvartaHjarta <8724275+SvartaHjarta@users.noreply.github.com>
2022-04-29 16:25:17 +0000

    minor tweaks to labeler and added base for db schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- T-220-VLN2 - Group 2
+--
+-- Schema for FireSale!
+--
+-- Helgi Hákonarson <helgihak20@ru.is>
+-- Konráð Elí Sigurgeirsson <konrad21@ru.is>
+-- Patrekur Þór Agnarsson <patrekur20@ru.is>
+-- Sigurður Baldvin Friðriksson <sigurdurf21@ru.is>
+
+-- TODO: create schema
