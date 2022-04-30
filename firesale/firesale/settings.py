6d79652c618a268e2b3160a67e3838fdc634c9b6
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-04-30 10:23:40 +0000

    Fixed more naming

--- firesale/firesale/settings.py
+++ firesale/firesale/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
