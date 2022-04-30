11bd18c04dbb184e117db94e390be8d0efb9472f
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-04-30 09:23:16 +0000

    Fixed naming

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Inside the firesale/firesale directory, please add your own .env file with the following information
 ```
 SECRET_KEY=
-DB_URL=
+DB_NAME=
 DB_USER=
 DB_PASSWORD=
 ```
--- firesale/firesale/settings.py
+++ firesale/firesale/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'firesale.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
     }
 }
 
