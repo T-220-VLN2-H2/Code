d0222327c7f2af7486a240365331e1a3b10ad632
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-04-30 09:20:13 +0000

    Database connection setup with env file. Some docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FireSale! codebase
+
+
+# Environment setup
+Inside the firesale/firesale directory, please add your own .env file with the following information
+```
+SECRET_KEY=
+DB_URL=
+DB_USER=
+DB_PASSWORD=
+```
--- firesale/firesale/settings.py
+++ firesale/firesale/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zsb-tme@tfss9ugcg^p6f*affb^#xx$ro$1^qo84j26e^r0dbf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'firesale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
     }
 }
 
