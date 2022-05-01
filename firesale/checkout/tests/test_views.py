040a2d3fa3ddc55ad43d18063306d73811acfeef
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-05-01 04:14:57 +0000

    Added checkout module with inital tests

---
+++ firesale/checkout/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class StatusCodeTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        print("Creates correct test data ")
+        pass
+
+    def setUp(self) -> None:
+        print("Setup, run once for each test")
+        pass
+
+    def test_catalog_returns_200(self):
+        response = self.client.get('/catalog')
+        self.assertEqual(response.status_code, 200)
+
