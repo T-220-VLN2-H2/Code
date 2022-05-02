56bbe852af9b94a5e27c4f405622b541c1d93357
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-05-02 11:36:16 +0000

    Added tests for categories

--- core/tests/test_categories.py
+++ core/tests/test_categories.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 
 
-class StatusCodeTest(TestCase):
+class CategoryStatusAndContentTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("Creates correct test data ")
-        pass
+        print("Adding test categories")
+        cls.categories = [
+            {"id": 1, "name": "Electronics"},
+            {"id": 2, "name": "Furniture"}
+        ]
 
-    def setUp(self) -> None:
-        print("Setup, run once for each test")
-        pass
+    # def setUp(self) -> None:
+    #    print("Setup, run once for each test")
+    #    pass
 
     def test_category_returns_200(self):
         response = self.client.get('/category')
         self.assertEqual(response.status_code, 200)
+
+    def test_category_contains_category_names(self):
+        response = self.client.get('/category')
+        for cat in self.categories:
+            self.assertContains(response, f'<p>{cat["name"]}</p>')
+
 
