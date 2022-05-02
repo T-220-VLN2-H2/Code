f132273fd1683d688a5df887037085f91acfd8b7
SvartaHjarta <8724275+SvartaHjarta@users.noreply.github.com>
2022-05-02 16:00:43 +0000

    stared db schema, should probably just parse from report

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,38 @@
 -- Sigurður Baldvin Friðriksson <sigurdurf21@ru.is>
 
 -- TODO: create schema
+
+-- drop table if exists  cascade;
+
+drop table if exists users cascade;
+
+create table users (
+  id serial primary key,
+  full_name varchar not null,
+  user_name varchar not null,
+  email varchar not null,
+  password_hash varchar not null
+);
+
+drop table if exists notifications cascade;
+create table notifications (
+  id serial primary key,
+  title varchar not null,
+  message varchar not null,
+  time timestamp
+);
+
+drop table if exists search_history cascade;
+create table search_history (
+  search_string varchar,
+  time timestamp
+  -- TODO: add pk (search_string, time)
+);
+
+drop table if exists items cascade;
+create table items (
+  id serial primary key,
+  price deciamal, -- null if auction
+  condition
+
+);
