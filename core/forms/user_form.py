e37836eb5eab61315a1cefe27ed68fc61d003f99
Vixby <helgihak@gmail.com>
2022-05-03 10:32:31 +0000

    User model starting to emerge

---
+++ core/forms/user_form.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm, widgets
+from core.models import User
+
+class UserCreateForm(ModelForm):
+    class Meta:
+        model = User
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'user_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control'}),
+            'bio': widgets.TextInput(attrs={'class': 'form-control'})
+        }
