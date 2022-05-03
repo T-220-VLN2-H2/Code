d58e6bfe45b8e7f666b6caf0b3392bc4fbaa8964
Lint Action <lint-action@samuelmeuli.com>
2022-05-03 14:52:08 +0000

    Fix code style issues with Black

--- core/urls/categories.py
+++ core/urls/categories.py
@@ -3,6 +3,6 @@ from django.urls import path
 from ..views import categories
 
 urlpatterns = [
-    path('category', categories.home, name='category_home'),
-    path('category/<cat_id>', categories.category_page, name="category_page")
+    path("category", categories.home, name="category_home"),
+    path("category/<cat_id>", categories.category_page, name="category_page"),
 ]
--- core/views/categories.py
+++ core/views/categories.py
@@ -3,26 +3,23 @@ from .data import categories, items, categories_with_parents
 
 
 def home(request):
-    ctx = {
-        "categories": categories,
-        "sub_categories": categories_with_parents
-    }
-    return render(request, '../templates/categories/home.html', context=ctx)
+    ctx = {"categories": categories, "sub_categories": categories_with_parents}
+    return render(request, "../templates/categories/home.html", context=ctx)
 
 
 def category_page(request, cat_id):
 
-    ctx = {
-        "categories": categories,
-        "sub_categories": categories_with_parents
-    }
+    ctx = {"categories": categories, "sub_categories": categories_with_parents}
 
     for cat in categories:
-        if cat['id'] == int(cat_id):
+        if cat["id"] == int(cat_id):
             ctx["selected_category"] = cat
-            ctx["category_items"] = [item for item in items if item['category'] == cat['id']]
-            if cat['id'] in categories_with_parents:
-                for sub_cat in categories_with_parents[cat['id']]:
-                    ctx["category_items"] += [item for item in items if item['category'] == sub_cat['id']]
-    return render(request, '../templates/categories/category_page.html', context=ctx)
-
+            ctx["category_items"] = [
+                item for item in items if item["category"] == cat["id"]
+            ]
+            if cat["id"] in categories_with_parents:
+                for sub_cat in categories_with_parents[cat["id"]]:
+                    ctx["category_items"] += [
+                        item for item in items if item["category"] == sub_cat["id"]
+                    ]
+    return render(request, "../templates/categories/category_page.html", context=ctx)
