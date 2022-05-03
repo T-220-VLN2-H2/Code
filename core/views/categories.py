a5a42ef5a507e5f5509fa93ce597e78341c4f5b3
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-05-03 09:59:06 +0000

    Added sidebar, bootstrap import etc.

--- core/views/categories.py
+++ core/views/categories.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 
-
-def home(request):
-    categories = [
+categories = [
         {"id": 1, "name": "Electronics"},
         {"id": 2, "name": "Furniture"}
     ]
+
+
+def home(request):
     ctx = {
         "categories": categories
     }
     return render(request, '../templates/categories/home.html', context=ctx)
+
+
+def category_page(request, id):
+    ctx = {
+        "categories": categories
+    }
+    for cat in categories:
+        if cat['id'] == int(id):
+            ctx["selected_category"] = cat
+            ctx["category_items"] = [{"id": 1, "name": cat['name']+" item"}]
+            print(ctx)
+
+    return render(request, '../templates/categories/category_page.html', context=ctx)
+
