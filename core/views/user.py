b9bc34f53784652cb7a00a37d86bc1d0826bc8b9
Patrekur <Patrekur20@ru.is>
2022-05-03 13:49:14 +0000

    Change user to users, added base elements to user history and profile.

---
+++ core/views/user.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+folder_path = '../templates/user'
+
+categories = [
+        {"id": 1, "name": "Electronics"},
+        {"id": 2, "name": "Furniture"}
+    ]
+
+def home(request):
+    ctx = {
+        "categories": categories
+    }
+
+    return render(request, f'{folder_path}/index.html', context=ctx)
+
+def edit(request):
+
+    return render(request, f'{folder_path}/edit.html')
+
+def profile(request, id):
+    users = [
+        {"id": 1, "name": "John Doe"},
+        {"id": 2, "name": "Helgi Hak"},
+    ]
+    ctx = None
+    for user in users:
+        if user["id"] == int(id):
+            ctx = user
+            break
+
+    return render(request, f'{folder_path}/user.html', context=ctx)
+
+def history(request, id):
+    ctx = {
+        "categories": categories
+    }
+
+    return render(request, f'{folder_path}/history.html', context=ctx)
+
+def messages(request, id):
+    return render(request, f'{folder_path}/messages.html')
+
--- firesale/urls.py
+++ firesale/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls.categories')),
-    path('', include('core.urls.users'))
+    path('', include('core.urls.user'))
 ]
