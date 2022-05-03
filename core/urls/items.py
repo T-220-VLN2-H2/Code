7f6858b36eb38937bfdd502dc1a7c64d897ceffd
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-05-03 13:45:36 +0000

    Added some functionality * Category tree with 1 child * Category view displays items beloning to children categories * Some product page templating

---
+++ core/urls/items.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from ..views import items
+
+urlpatterns = [
+    path('items', items.items_page, name='items'),
+    path('items/<item_id>', items.item_details, name="item_details")
+]
