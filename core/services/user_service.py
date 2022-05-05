49a73b1fec044ff769e1133c4388d4447e8375d2
Vixby <helgihak@gmail.com>
2022-05-05 13:22:29 +0000

    working on create user mainly

--- core/services/user_service.py
+++ core/services/user_service.py
@@ -1,20 +1,33 @@
-from django.shortcuts import redirect, render
-from core.models.user import User
+from core.models.user import Profile
 
 class UserService:
     def get_user_info(user_id):
-        print('do something')
-        #TODO validate the request, find the corresponding user in DB and return object. Else raise error.
-    
-    def set_user_info(id, **kwargs):
-        print('do something')
+        user = Profile.objects.filter(id = user_id)
+        return user
+
+    def set_user_info(self, user_id, **kwargs):
+        user = self.get_user_info(user_id)
+        updated_information = []
+        for item in kwargs.items():
+            updated_information.append(item)
+        print(updated_information)
+        for item in updated_information:
+            user.item
         #TODO validate the request, find the corresponding user in DB and update information. Else raise error.
 
-    def delete_user(user_id):
-        print(1)
-        #TODO validate the request, forward to DB to remove information, return success. Else raise error.
+    # nauðsynlegt?
+    # def delete_user(user_id):
+    #     print(1)
+    #     #TODO validate the request, forward to DB to remove information, return success. Else raise error.
         
 
-    def create_user(**kwargs):
-        new_user = User(**kwargs)
+    def create_user(username, first_name, last_name, email, password):
+        new_user = Profile(username, first_name, last_name, email, password)
         new_user.save()
+        #return redirect() #finna url til að redirecta
+
+def main():
+    new_user = UserService.create_user('Hellibelli', 'Helgi', 'Hákonarson', 'Helgihak@gmail.com', 'abc123')
+
+if __name__ == "__main__":
+    main()
