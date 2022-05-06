bf7cc6eb8531fb6d03a04d2634727ef385b2e5a4
Lint Action <lint-action@samuelmeuli.com>
2022-05-06 13:50:27 +0000

    Fix code style issues with ESLint

--- core/static/js/searchResults.js
+++ core/static/js/searchResults.js
@@ -1,45 +1,44 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 async function search() {
-    const search = async (q) => {
-        let result = await fetch(`/search`, {
-            method: "GET"
-        }).then((r) => r.json())
-        return result
+  const search = async (q) => {
+    const result = await fetch(`/search`, {
+      method: 'GET',
+    }).then((r) => r.json());
+    return result;
+  };
+  const clearChildren = async (parent) => {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
     }
-    const clearChildren = async (parent) => {
-        while(parent.firstChild){
-            parent.removeChild(parent.firstChild)
-        }
-    }
-    const searchDrop = document.getElementById("myDropdown");
-    await clearChildren(searchDrop)
-    const searchInput = document.getElementById("myInput");
-    let searchResult = await search(searchInput.value);
-    for (let i = 0; i < searchResult.length; i++){
-        const a = document.createElement("a");
-        a.text = `${searchResult[i].title} price: ${searchResult[i].price}`
-        a.href = `/items/${searchResult[i].id}`
-        searchDrop.appendChild(a)
-    }
-
+  };
+  const searchDrop = document.getElementById('myDropdown');
+  await clearChildren(searchDrop);
+  const searchInput = document.getElementById('myInput');
+  const searchResult = await search(searchInput.value);
+  for (let i = 0; i < searchResult.length; i++) {
+    const a = document.createElement('a');
+    a.text = `${searchResult[i].title} price: ${searchResult[i].price}`;
+    a.href = `/items/${searchResult[i].id}`;
+    searchDrop.appendChild(a);
+  }
 }
 const toggleSearchDrop = () => {
-    const searchDrop = document.getElementById("myDropdown");
-    searchDrop.classList.toggle("show");
-}
+  const searchDrop = document.getElementById('myDropdown');
+  searchDrop.classList.toggle('show');
+};
 async function filterFunction() {
-      let input, filter, ul, li, a, i;
-      input = document.getElementById("myInput");
-      filter = input.value.toUpperCase();
-      let div = document.getElementById("myDropdown");
-      a = div.getElementsByTagName("a");
-      for (i = 0; i < a.length; i++) {
-          let txtValue = a[i].textContent || a[i].innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          a[i].style.display = "";
-          } else {
-            a[i].style.display = "none";
-          }
-      }
+  let input; let filter; let ul; let li; let a; let i;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  const div = document.getElementById('myDropdown');
+  a = div.getElementsByTagName('a');
+  for (i = 0; i < a.length; i++) {
+    const txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = '';
+    } else {
+      a[i].style.display = 'none';
+    }
+  }
 }
