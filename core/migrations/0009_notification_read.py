b4c3f59b3c255d358f7abffd43354a08b3d60b87
Lint Action <lint-action@samuelmeuli.com>
2022-05-08 23:07:21 +0000

    Fix code style issues with Black

--- core/migrations/0009_notification_read.py
+++ core/migrations/0009_notification_read.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0008_alter_profile_table'),
+        ("core", "0008_alter_profile_table"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='notification',
-            name='read',
+            model_name="notification",
+            name="read",
             field=models.BooleanField(default=False),
         ),
     ]
