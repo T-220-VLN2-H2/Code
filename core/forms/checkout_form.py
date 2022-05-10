da801aadf362ee227db4cab4fc925a91c453d097
Vixby <helgihak@gmail.com>
2022-05-10 12:08:56 +0000

    Changing arrangements and url path names for checkout

--- core/forms/checkout_form.py
+++ core/forms/checkout_form.py
@@ -13,7 +13,7 @@ class PaymentCreateForm(ModelForm):
             "expiry_year",
         ]
         widgets = {
-            "name": widgets.TextInput(attrs={"class": "form-control"}),
+            "cardholder_name": widgets.TextInput(attrs={"class": "form-control"}),
             "card_number": widgets.TextInput(attrs={"class": "form-control"}),
             "cvc": widgets.TextInput(attrs={"class": "form-control"}),
             "expiry_month": widgets.NumberInput(attrs={"class": "form-control"}),
@@ -24,12 +24,15 @@ class PaymentCreateForm(ModelForm):
 class PersonalInfoCreateForm(ModelForm):
     class Meta:
         model = ShippingDetails
-        fields = ["full_name", "address", "postal_code", "city"]
+        fields = [
+            "full_name", 
+            "address", 
+            "postal_code", 
+            "city"
+        ]
         widgets = {
-            "first_name": widgets.TextInput(attrs={"class": "form-control"}),
-            "last_name": widgets.TextInput(attrs={"class": "form-control"}),
+            "full_name": widgets.TextInput(attrs={"class": "form-control"}),
             "address": widgets.TextInput(attrs={"class": "form-control"}),
-            "Zip": widgets.NumberInput(attrs={"class": "form-control"}),
+            "postal_code": widgets.NumberInput(attrs={"class": "form-control"}),
             "city": widgets.TextInput(attrs={"class": "form-control"}),
-            "Country": widgets.TextInput(attrs={"class": "form-control"}),
         }
--- core/views/checkout.py
+++ core/views/checkout.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from core.forms.checkout_form import PersonalInfoCreateForm
+from core.forms.checkout_form import PaymentCreateForm, PersonalInfoCreateForm
 from core.services.checkout_service import CheckoutService
 
 
-def personal_info(request):
+def user(request):
     folder_path = "../templates/"
     checkout_service = CheckoutService()
     ctx = {
@@ -14,30 +14,47 @@ def personal_info(request):
         form = PersonalInfoCreateForm(request.post)
         if form.is_valid():
             new_personl_info = form.save()
-    return render(request, "checkout/personal_info.html", context=ctx)
+    else:
+        form = PersonalInfoCreateForm()
+        ctx["form"] = form
+    return render(request, "checkout/user_details.html", context=ctx)
 
 
-def delivery_info(request):
+def delivery(request):
     folder_path = "../templates/"
     checkout_service = CheckoutService()
     ctx = {
         "full_name": "",
         "address": "",
     }
-    return render(request, "checkout/summary.html", context=ctx)
+    if request.method == "POST":
+        form = PaymentCreateForm(request.post)
+        if form.is_valid():
+            new_personl_info = form.save()
+    else:
+        form = PaymentCreateForm()
+        ctx["form"] = form
+    return render(request, "checkout/payment.html", context=ctx)
 
 
-def payment_info(request):
+def payment(request):
     folder_path = "../templates/"
     checkout_service = CheckoutService()
     ctx = {
         "full_name": "",
         "address": "",
     }
+    if request.method == "POST":
+        form = PaymentCreateForm(request.post)
+        if form.is_valid():
+            new_personl_info = form.save()
+    else:
+        form = PaymentCreateForm()
+        ctx["form"] = form
     return render(request, "checkout/payment_info.html", context=ctx)
 
 
-def summary_display(request):
+def summary(request):
     folder_path = "../templates/"
     checkout_service = CheckoutService()
     ctx = {
