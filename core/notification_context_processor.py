bc5ee8bbb66a77c2b898384dfb29ec54919f5652
Lint Action <lint-action@samuelmeuli.com>
2022-05-10 11:29:40 +0000

    Fix code style issues with Black

--- core/notification_context_processor.py
+++ core/notification_context_processor.py
@@ -4,5 +4,7 @@ from core.services.notification_service import NotificationService
 def notifications(request):
     notify_service = NotificationService()
     return {
-        "notifications": notify_service.get_notifications(request.user, only_unread=True)
+        "notifications": notify_service.get_notifications(
+            request.user, only_unread=True
+        )
     }
