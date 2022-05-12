5b5861314e6b4bad4e95b563ce741da759c206e0
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2022-05-12 13:23:59 +0000

    deployment

--- firesale/settings.py
+++ firesale/settings.py
@@ -20,6 +20,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
-ALLOWED_HOSTS = []
 
 LOGOUT_REDIRECT_URL = "/"
 
