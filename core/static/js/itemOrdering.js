da47c623505d2b513e79642a5e844e1210435995
Lint Action <lint-action@samuelmeuli.com>
2022-05-13 10:20:43 +0000

    Fix code style issues with ESLint

--- core/static/js/itemOrdering.js
+++ core/static/js/itemOrdering.js
@@ -1,31 +1,31 @@
 const filter = document.getElementById('orderBy');
 window.addEventListener('load', function() {
-    const query_params = window.location.search.split('&')
-    const selected = query_params[0].split("=")[1];
-    let query = "";
-    try{
-        query =  query_params[1].split("=")[1];
-    } catch {
+  const query_params = window.location.search.split('&');
+  const selected = query_params[0].split('=')[1];
+  let query = '';
+  try {
+    query = query_params[1].split('=')[1];
+  } catch {
 
-    }
-    let query_field = document.getElementById("searchQuery")
-    try {
-        query_field.value = query
-    } catch{
+  }
+  const query_field = document.getElementById('searchQuery');
+  try {
+    query_field.value = query;
+  } catch {
 
-    }
+  }
 
-    if (selected !== undefined){
-        filter.value = selected;
+  if (selected !== undefined) {
+    filter.value = selected;
   }
 });
 
 filter.addEventListener('change', (e) => {
-    let query = "";
-    try{
-        query = document.getElementById("searchQuery").value;
-    } catch {
-        query = ""
-    }
-    window.location.href = `?sort=${filter.value}&query=${query}`;
+  let query = '';
+  try {
+    query = document.getElementById('searchQuery').value;
+  } catch {
+    query = '';
+  }
+  window.location.href = `?sort=${filter.value}&query=${query}`;
 });
