789a1d7bad2144ca75d7ca83f5dee074c5bcc145
SvartaHjarta <8724275+SvartaHjarta@users.noreply.github.com>
2022-05-13 14:28:20 +0000

    fixed rating in item detail

--- core/views/items.py
+++ core/views/items.py
@@ -23,9 +23,12 @@ def item_details(request, item_id):
     # TODO: fix late update when bidding
     max_bid = BidService.get_max_bid(item_id)
     max_bid = max_bid.amount if max_bid is not None else 0
-    seller_avg_rating = UserService.get_user_rating(item.seller.id)["rating__avg"]
+
+    seller_avg_rating = UserService.get_user_rating(item.seller.id)
+
     if seller_avg_rating and seller_avg_rating.is_integer():
         seller_avg_rating = int(seller_avg_rating)
+
     ctx["seller_avg_rating"] = seller_avg_rating
     ctx["images"] = ImageService.get_images(ctx["item"])
     ctx["similar_items"] = ItemService.get_similar_items(ctx["item"])
